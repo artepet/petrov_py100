81e7a8751b9a6afa7f0e76f83fbfadbe38d540be
Artem <115809043+artepet@users.noreply.github.com>
2022-11-10 16:13:09 +0300

    Лабораторная 4 тема 5
    в task1 явно сделал не то что надо было

---
+++ Лабораторная работа 4/task1.py
@@ -0,0 +1,36 @@
+# TODO решить с помощью list comprehension и распечатать его
+import pprint
+
+dict_sample_0 = {'bin': bin (0), 'dec': 0, 'hex': hex (0), 'oct': oct (0)}
+dict_sample_1 = {'bin': bin (1), 'dec': 1, 'hex': hex (1), 'oct': oct (1)}
+dict_sample_2 = {'bin': bin (2), 'dec': 2, 'hex': hex (2), 'oct': oct (2)}
+dict_sample_3 = {'bin': bin (3), 'dec': 3, 'hex': hex (3), 'oct': oct (3)}
+dict_sample_4 = {'bin': bin (4), 'dec': 4, 'hex': hex (4), 'oct': oct (4)}
+dict_sample_5 = {'bin': bin (5), 'dec': 5, 'hex': hex (5), 'oct': oct (5)}
+dict_sample_6 = {'bin': bin (6), 'dec': 6, 'hex': hex (6), 'oct': oct (6)}
+dict_sample_7 = {'bin': bin (7), 'dec': 7, 'hex': hex (7), 'oct': oct (7)}
+dict_sample_8 = {'bin': bin (8), 'dec': 8, 'hex': hex (8), 'oct': oct (8)}
+dict_sample_9 = {'bin': bin (9), 'dec': 9, 'hex': hex (9), 'oct': oct (9)}
+dict_sample_10 = {'bin': bin (10), 'dec': 10, 'hex': hex (10), 'oct': oct (10)}
+dict_sample_11 = {'bin': bin (11), 'dec': 11, 'hex': hex (11), 'oct': oct (11)}
+dict_sample_12 = {'bin': bin (12), 'dec': 12, 'hex': hex (12), 'oct': oct (12)}
+dict_sample_13 = {'bin': bin (13), 'dec': 13, 'hex': hex (13), 'oct': oct (13)}
+dict_sample_14 = {'bin': bin (14), 'dec': 14, 'hex': hex (14), 'oct': oct (14)}
+dict_sample_15 = {'bin': bin (15), 'dec': 15, 'hex': hex (15), 'oct': oct (15)}
+
+print(dict_sample_0)
+print(dict_sample_1)
+print(dict_sample_2)
+print(dict_sample_3)
+print(dict_sample_4)
+print(dict_sample_5)
+print(dict_sample_6)
+print(dict_sample_7)
+print(dict_sample_8)
+print(dict_sample_9)
+print(dict_sample_10)
+print(dict_sample_11)
+print(dict_sample_12)
+print(dict_sample_13)
+print(dict_sample_14)
+print(dict_sample_15)
---
+++ Лабораторная работа 4/task3.py
@@ -0,0 +1,7 @@
+def get_unique_list_numbers() -> list[int]:
+    ...  # TODO написать функцию для получения списка уникальных целых чисел
+
+
+from random import sample
+list = sample(range(-10, 11), 15)
+print(list)
